d44ee9f26b947861bf7fafc011eb19f7040aae73
Molly Wobbles <molly.rendon@outlook.com>
2022-03-19 10:33:48 -0400

    Updated file structure and added pages

---
+++ client/src/page/Meetups/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.css";
+
+const Meetups = () => {
+    //Javascript goes here
+    return
+    <div>
+        Meetups
+    </div>
+}
+
+export default Meetups;
