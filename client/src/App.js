4dd2666cbfec539bb897072b8ad5d93ad3ac72c6
Molly Wobbles <molly.rendon@outlook.com>
2022-03-19 21:23:43 -0400

    Added imports to App.js file

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,18 @@ import React from 'react';
 import './App.css';
 
 import Navbar from './components/Navbar';
+import Main from './components/Main';
+import Dashboard from './page/Dashboard';
+import Meetups from './page/Meetups';
 
 
 function App() {
   return (
     <div>
       <Navbar />
+      <Main />
+      <Dashboard />
+      <Meetups />
     </div>
   );
 }
