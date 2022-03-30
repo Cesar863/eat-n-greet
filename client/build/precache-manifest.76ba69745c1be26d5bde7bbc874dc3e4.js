35f34e0a758014930ac78531d3aab90c8d7b2488
Cesar863 <90540881+Cesar863@users.noreply.github.com>
2022-03-29 21:14:41 -0400

    client build index

---
+++ client/build/precache-manifest.76ba69745c1be26d5bde7bbc874dc3e4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e23a6b2ec8785a002a86c7bb416ee3da",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5d0758050079cb2064ee",
+    "url": "/static/css/2.3661fc73.chunk.css"
+  },
+  {
+    "revision": "bd65a92f0d4a85c3cbf6",
+    "url": "/static/css/main.008e32c0.chunk.css"
+  },
+  {
+    "revision": "5d0758050079cb2064ee",
+    "url": "/static/js/2.06f1e57a.chunk.js"
+  },
+  {
+    "revision": "4ebd2e1c8aec2794a625d08d8b03eea9",
+    "url": "/static/js/2.06f1e57a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd65a92f0d4a85c3cbf6",
+    "url": "/static/js/main.b5ee857e.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
